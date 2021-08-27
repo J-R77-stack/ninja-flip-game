8aacadc699533910503e139913035fbd63822f57
J-R77-stack <johnnyramsay81@gmail.com>
2021-08-27 13:50:56 +0000

    create function constructor for totalTime and cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,6 +31,18 @@ class AudioController {
     }
 }
 
+class NinjaFlip {
+    constructor(totalTime, cards){
+        this.cardsArray = cards;
+        this.totalTime = totalTime;
+        this.timeRemaining = totalTime;
+        this.timer = document.getElementById('time-remaining');
+        this.flipper = document.getElementById('flips');
+        this.audioController = new AudioController();
+    }
+   
+}
+
 function ready() {
     let overlays = Array.from(document.getElementsByClassName('overlay-text'));
     let cards = Array.from(document.getElementsByClassName('card'));
@@ -39,8 +51,7 @@ function ready() {
         overlay.addEventListener('click', () => {
             overlay.classList.remove('visible');
             // game.startGame();
-            let audioController = new AudioController();
-audioController.startMusic();
+           
         });
     });
     cards.forEach(card => {
