0e1d967bd78e3435238bc3efaa468308b7b25b4f
J-R77-stack <johnnyramsay81@gmail.com>
2021-08-27 13:10:15 +0000

    Add fuction to flip card on eventlistener of click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,20 @@
-if(document.readyState === 'loading') {
-    document.addEventListener('DOMContentloaded', ready());
-} else {
-    ready();
+class AudioController {
+    constructor() {
+        this.backingMusic = new Audio('assets/audio/background.mp3');
+        this.flipSound = new Audio('assets/audio/flip.wav');
+        this.matchSound = new Audio('assets/audio/match.mp3');
+        this.winnerSound = new Audio('assets/audio/winner.mp3');
+        this.gameoverSound = new Audio('assets/audio/game_over.mp3');
+        this.backingMusic.volume = 0.5;
+        this.backingMusic.loop = true;
+    }
+    startMusic() {
+        this.backingMusic.play();
+    }
+    stopMusic() {
+        this.backingMusic.pause();
+        this.backingMusic.currentTime = 0;
+    }
 }
 
 function ready() {
@@ -11,6 +24,21 @@ function ready() {
     overlays.forEach(overlay => {
         overlay.addEventListener('click', () => {
             overlay.classList.remove('visible');
+            // game.startGame();
+            let audioController = new AudioController();
+audioController.startMusic();
+        });
+    });
+    cards.forEach(card => {
+        card.addEventListener('click', () => {
+            // game.flipCard(card);
         });
     });
 }
+
+if(document.readyState === 'loading') {
+    document.addEventListener('DOMContentloaded', ready());
+} else {
+    ready();
+}
+
